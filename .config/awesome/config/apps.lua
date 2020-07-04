4d8b54a24ab6f9e2a2428357287e8847c0605898
Bradly Strachan <strachanbrad@protonmail.com>
2020-07-03 23:25:50 -0300

    fragmenting rc.lua

---
+++ .config/awesome/config/apps.lua
@@ -0,0 +1,8 @@
+return {
+	terminal = "alacritty",
+	editor = os.getenv("EDITOR") or "nvim",
+	browser = "vimb",
+	file_manager = "vifm",
+	notes = "joplin-desktop",
+	launcher = "rofi -show drun -modi drun -theme ~/.config/rofi/themes/dmenu", 
+}
---
+++ .config/awesome/config/globalkeys.lua
@@ -0,0 +1,157 @@
+local awful = require('awful')
+local apps = require('config.apps')
+local hotkeys_popup = require('awful.hotkeys_popup').widget
+
+local modkey = require('config.mod').modkey
+
+awful.keyboard.append_global_keybindings({
+	awful.key({ modkey,           }, "s",      hotkeys_popup.show_help,
+		{description="show help", group="awesome"}),
+	awful.key({ modkey,           }, "w", function () mymainmenu:show() end,
+		{description = "show main menu", group = "awesome"}),
+	awful.key({ modkey, "Control" }, "r", awesome.restart,
+		{description = "reload awesome", group = "awesome"}),
+	awful.key({ modkey, "Shift"   }, "q", awesome.quit,
+		{description = "quit awesome", group = "awesome"}),
+
+	-- Launcher related keybindings
+	awful.key({ modkey,           }, "Return", function () awful.spawn(apps.terminal) end,
+		{description = "open a terminal", group = "launcher"}),
+	awful.key({ modkey,           },            "r",     function () awful.spawn(apps.launcher) end,
+		{description = "run launcher", group = "launcher"}),
+	awful.key({ modkey },            "e",     function () awful.spawn(apps.terminal .. " -e " .. apps.editor) end,
+		{description = "launch editor", group = "launcher"}),
+	awful.key({ modkey },            "b",     function () awful.spawn(apps.browser) end,
+		{description = "launch browser", group = "launcher"}),
+	awful.key({ modkey },            "f",     function () awful.spawn(apps.terminal .. " -e " .. apps.file_manager) end,
+		{description = "launch file manager", group = "launcher"}),
+	awful.key({ modkey },            "n",     function () awful.spawn(apps.notes) end,
+		{description = "launch editor", group = "launcher"}),
+
+	-- Tags related keybindings
+	awful.key({ modkey, "Control" }, "h",   awful.tag.viewprev,
+		{description = "view previous", group = "tag"}),
+	awful.key({ modkey, "Control" }, "l",  awful.tag.viewnext,
+		{description = "view next", group = "tag"}),
+	awful.key({ modkey,           }, "Escape", awful.tag.history.restore,
+		{description = "go back", group = "tag"}),
+
+	-- Focus related keybindings
+	awful.key({ modkey,           }, "j",
+		function ()
+			awful.client.focus.byidx( 1)
+		end,
+		{description = "focus next by index", group = "client"}
+		),
+	awful.key({ modkey,           }, "k",
+		function ()
+			awful.client.focus.byidx(-1)
+		end,
+		{description = "focus previous by index", group = "client"}
+		),
+	awful.key({ modkey,           }, "Tab",
+		function ()
+			awful.client.focus.history.previous()
+			if client.focus then
+				client.focus:raise()
+			end
+		end,
+		{description = "go back", group = "client"}),
+	awful.key({ modkey, "Control" }, "j", function () awful.screen.focus_relative( 1) end,
+		{description = "focus the next screen", group = "screen"}),
+	awful.key({ modkey, "Control" }, "k", function () awful.screen.focus_relative(-1) end,
+		{description = "focus the previous screen", group = "screen"}),
+
+	-- Layout related keybindings
+	awful.key({ modkey, "Shift"   }, "j", function () awful.client.swap.byidx(  1)    end,
+		{description = "swap with next client by index", group = "client"}),
+	awful.key({ modkey, "Shift"   }, "k", function () awful.client.swap.byidx( -1)    end,
+		{description = "swap with previous client by index", group = "client"}),
+	awful.key({ modkey,           }, "u", awful.client.urgent.jumpto,
+		{description = "jump to urgent client", group = "client"}),
+	awful.key({ modkey,           }, "l",     function () awful.tag.incmwfact( 0.05)          end,
+		{description = "increase master width factor", group = "layout"}),
+	awful.key({ modkey,           }, "h",     function () awful.tag.incmwfact(-0.05)          end,
+		{description = "decrease master width factor", group = "layout"}),
+	awful.key({ modkey, "Shift"   }, "h",     function () awful.tag.incnmaster( 1, nil, true) end,
+		{description = "increase the number of master clients", group = "layout"}),
+	awful.key({ modkey, "Shift"   }, "l",     function () awful.tag.incnmaster(-1, nil, true) end,
+		{description = "decrease the number of master clients", group = "layout"}),
+	awful.key({ modkey, "Control" }, "h",     function () awful.tag.incncol( 1, nil, true)    end,
+		{description = "increase the number of columns", group = "layout"}),
+	awful.key({ modkey, "Control" }, "l",     function () awful.tag.incncol(-1, nil, true)    end,
+		{description = "decrease the number of columns", group = "layout"}),
+	awful.key({ modkey,           }, "space", function () awful.layout.inc( 1)                end,
+		{description = "select next", group = "layout"}),
+	awful.key({ modkey, "Shift"   }, "space", function () awful.layout.inc(-1)                end,
+		{description = "select previous", group = "layout"}),
+
+
+	awful.key {
+		modifiers   = { modkey },
+		keygroup    = "numrow",
+		description = "only view tag",
+		group       = "tag",
+		on_press    = function (index)
+			local screen = awful.screen.focused()
+			local tag = screen.tags[index]
+			if tag then
+				tag:view_only()
+			end
+		end,
+	},
+	awful.key {
+		modifiers   = { modkey, "Control" },
+		keygroup    = "numrow",
+		description = "toggle tag",
+		group       = "tag",
+		on_press    = function (index)
+			local screen = awful.screen.focused()
+			local tag = screen.tags[index]
+			if tag then
+				awful.tag.viewtoggle(tag)
+			end
+		end,
+	},
+	awful.key {
+		modifiers = { modkey, "Shift" },
+		keygroup    = "numrow",
+		description = "move focused client to tag",
+		group       = "tag",
+		on_press    = function (index)
+			if client.focus then
+				local tag = client.focus.screen.tags[index]
+				if tag then
+					client.focus:move_to_tag(tag)
+				end
+			end
+		end,
+	},
+	awful.key {
+		modifiers   = { modkey, "Control", "Shift" },
+		keygroup    = "numrow",
+		description = "toggle focused client on tag",
+		group       = "tag",
+		on_press    = function (index)
+			if client.focus then
+				local tag = client.focus.screen.tags[index]
+				if tag then
+					client.focus:toggle_tag(tag)
+				end
+			end
+		end,
+	},
+	awful.key {
+		modifiers   = { modkey },
+		keygroup    = "numpad",
+		description = "select layout directly",
+		group       = "layout",
+		on_press    = function (index)
+			local t = awful.screen.focused().selected_tag
+			if t then
+				t.layout = t.layouts[index] or t.layout
+			end
+		end,
+	}
+})
+
